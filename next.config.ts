6f6e566e796a2c0cde2a830f02cb6c7c01aed450
Vito Elias <vito.elias9@gmail.com>
2025-02-20 15:08:00 -0300

    feat: add initial restaurant page and menu page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }, { hostname: "s3-alpha-sig.figma.com" }],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+import { ChevronLeft, ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import ResturantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMehtod: string) => {
+  return consumptionMehtod === "DINE_IN" || consumptionMehtod === "TAKEAWAY";
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+  });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <ResturantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
