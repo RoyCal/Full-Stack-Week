5f89fd774428e6a63ebefa4a84c95f7222750f28
Vito Elias <vito.elias9@gmail.com>
2025-02-21 16:50:23 -0300

    feat: add scroll to product details

--- src/app/[slug]/menu/[productid]/components/product-details.tsx
+++ src/app/[slug]/menu/[productid]/components/product-details.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
 import { Prisma, Product, Restaurant } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
@@ -22,8 +23,8 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
     });
     const handlePlusClick = () => setQuantity((prev) => prev + 1);
     return ( 
-        <div className="relative z-50 rounded-t-3xl p-5 mt-[-1.5rem] flex-auto flex-col flex">
-            <div className="flex-auto">
+        <div className="relative z-50 rounded-t-3xl p-5 mt-[-1.5rem] flex-auto flex-col flex overflow-hidden">
+            <div className="flex-auto overflow-hidden">
                 <div className="flex items-center gap-1.5">
                     <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full"/>
                     <p className="gap-1 text-xs text-muted-foreground">{product.restaurant.name}</p>
@@ -31,7 +32,7 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
 
                 <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
 
-                <div className="flex items-center justify-between">
+                <div className="flex items-center justify-between mt-3">
                     <h3 className="text-xl font-semibold">{formatCurrency(product.price)}</h3>
                     <div className="flex items-center gap-3 text-center">
                         <Button variant="outline" className="h-8 w-8 rounded-xl" onClick={handleMinusClick}>
@@ -44,18 +45,24 @@ const ProductDetails = ({product}: ProductDetailsProps) => {
                     </div>
                 </div>
 
-                <div className="mt-6 space-y-3">
-                    <h4 className="font-semibold">Sobre</h4>
-                    <p className="text-sm text-muted-foreground">{product.description}</p>
-                </div>
-
-                <div className="mt-6 space-y-3">
-                    <div className="flex items-center gap-1.5">
-                        <ChefHatIcon size={18}/>
-                        <h4 className="font-semibold">Ingredientes</h4>
+                <ScrollArea className="h-full">
+                    <div className="mt-6 space-y-3">
+                        <h4 className="font-semibold">Sobre</h4>
+                        <p className="text-sm text-muted-foreground">{product.description}</p>
                     </div>
-                    <p className="text-sm text-muted-foreground">{product.description}</p>
-                </div>
+
+                    <div className="mt-6 space-y-3">
+                        <div className="flex items-center gap-1.5">
+                            <ChefHatIcon size={18}/>
+                            <h4 className="font-semibold">Ingredientes</h4>
+                        </div>
+                        <ul className="list-disc px-5 text-sm text-muted-foreground">
+                            {product.ingredients.map((ingredient) => (
+                                <li key={ingredient}>{ingredient}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </ScrollArea>
             </div>
             
             <Button className="rounded-full w-full mt-6">Adicionar à sacola</Button>
