15de8bc0d0862753fe38d89b6066a6601381573e
Vito Elias <vito.elias9@gmail.com>
2025-02-26 14:52:36 -0300

    feat: add main page button to navigate to restaurant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-const HomePage = () => {
-  return <h1>hello world</h1>;
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+const HomePage = async () => {
+  const fswDonalds = await getRestaurantBySlug("fsw-donalds");
+  if(!fswDonalds) {
+    return notFound();
+  }
+  return (
+    <div className="flex flex-col justify-center h-full items-center">
+        <Link href="/fsw-donalds" className="border-2 p-8 rounded-full flex flex-col items-center gap-2 border-yellow-300">
+          <Image
+            src={fswDonalds.avatarImageUrl}
+            alt={fswDonalds.name}
+            width={82}
+            height={82}/>
+          <p className="font-semibold text-xl">FSW DONALDS</p>
+        </Link>
+    </div>
+  );
 }
  
 export default HomePage;
