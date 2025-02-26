c8c22be52808bca3c2a17a809eacc45e286e4707
Vito Elias <vito.elias9@gmail.com>
2025-02-26 16:10:47 -0300

    chore: move button to component for testing

---
+++ src/components/main-page-button.tsx
@@ -0,0 +1,25 @@
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+const MainPageButton = async () => {
+    const fswDonalds = await getRestaurantBySlug("fsw-donalds");
+    if(!fswDonalds) {
+        return notFound();
+    }
+    return ( 
+        <div className="flex flex-col justify-center h-full items-center">
+            <Link href="/fsw-donalds" className="border-2 p-8 rounded-full flex flex-col items-center gap-2 border-yellow-300">
+            <Image
+                src={fswDonalds.avatarImageUrl}
+                alt={fswDonalds.name}
+                width={82}
+                height={82}/>
+            <p className="font-semibold text-xl">FSW DONALDS</p>
+            </Link>
+        </div>
+    );
+}
+ 
+export default MainPageButton;
