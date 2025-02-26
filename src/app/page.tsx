b9066ab56c9353d52d4651b055642e4740b15118
Vito Elias <vito.elias9@gmail.com>
2025-02-26 16:17:10 -0300

    fix: do the simple

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import MainPageButton from "@/components/main-page-button";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <MainPageButton />
+    <div className="flex items-center justify-center h-screen">
+      <Link href="/fsw-donalds">
+        <p className="font-semibold text-xl border-2 border-black rounded-full p-3">FSW Donalds</p>
+      </Link>
+    </div>
   );
 }
  
